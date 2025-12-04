56f39e0d9d4f78528f46e993da649cc7b792bb11
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2025-12-04 16:50:05 -0500

    fix: update syncer tests

--- qb_site/syncer/tests/backfill/test_commit_backfill_only.py
+++ qb_site/syncer/tests/backfill/test_commit_backfill_only.py
@@ -20,7 +20,10 @@ class TestCommitBackfillOnly(TestCase):
     def _mk_pr(self, number: int, last_synced_at=None):
         if last_synced_at is None:
             last_synced_at = timezone.now()
-        return make_pr(self.repo, number, last_synced_at=last_synced_at)
+        pr = make_pr(self.repo, number, last_synced_at=last_synced_at)
+        pr.engagement_synced_at = last_synced_at
+        pr.save(update_fields=["engagement_synced_at"])
+        return pr
 
     @mock.patch("syncer.tasks.sync_tasks.GitHubClient")
     def test_commit_backfill_runs_when_up_to_date(self, MockClient) -> None:
