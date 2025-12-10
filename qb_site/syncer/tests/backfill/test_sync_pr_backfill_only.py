c5c52933eac75e8d2aa0142a1a32e6c07a1ef8cf
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2025-12-10 14:54:50 -0500

    fix: don't skip sync_pr if head_ci_status is missing; fix broken tests

--- qb_site/syncer/tests/backfill/test_sync_pr_backfill_only.py
+++ qb_site/syncer/tests/backfill/test_sync_pr_backfill_only.py
@@ -20,7 +20,8 @@ class TestSyncPrBackfillOnly(TestCase):
             last_synced_at = timezone.now()
         pr = make_pr(self.repo, number, last_synced_at=last_synced_at)
         pr.engagement_synced_at = last_synced_at
-        pr.save(update_fields=["engagement_synced_at"])
+        pr.head_ci_state = "SUCCESS"
+        pr.save(update_fields=["engagement_synced_at", "head_ci_state"])
         return pr
 
     @mock.patch("syncer.tasks.sync_tasks.GitHubClient")
